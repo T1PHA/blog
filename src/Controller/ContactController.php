baab4987f063c773883d0202deb6a21f050f4aaa
tipha <tiphaelflak@gmail.com>
2021-12-16 23:35:41 +0100

    TP SYMFONY FIN

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,27 +13,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contactez-nous", name="contact")
+     * @var ContactRepository
      */
-    public function index(): Response
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository)
+    {
+        $this->contactRepository = $contactRepository;
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request): Response
     {
         return $this->render('contact/index.html.twig', [
-            'name' => 'Alex',
+            "contact" => $this->contactRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/contacter/{city}", name="contactCity")
+     * @Route("/contact/{id}", name="contactId")
      */
-    public function contactCity(Request $request, string $city): Response
+    public function ContactIds(Request $request, int $id): Response
     {
-        $name = $request->query->get('name');
-
-        dump($name);
-
-        return $this->render('contact/index.html.twig', [
-            'name' => $name,
-            'city' => $city
+        //$name = $request->query->get('name');
+        return $this->render('pages/contact.html.twig', [
+            'contact' => $this->contactRepository->find($id)
         ]);
     }
 }
